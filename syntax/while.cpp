a3b51c6feedd65edcbbe72083b807efe096d9d51
Seia <30369714+Seia-Soto@users.noreply.github.com>
2019-02-09 05:42:47 +0900

    Update while.cpp
    [대입 연산자를 while에 쓰면 잘못된 결과가 일어날 수 있습니다. #2](https://github.com/Seia-Soto/cl-indexes/issues/2)
    
    - Edit loopback point

--- syntax/while.cpp
+++ syntax/while.cpp
@@ -6,11 +6,11 @@ int main(void) {
   /* In only-while statement, first check statement and execute. */
   while (i=0) {
     printf("Printing message because the variable 'i' is declared as zero and the condition is while statement is 'i=0'\n");
-    /* Infinity loop */
   }
 
   /* In do-while statement, first run and check statement. */
   do {
     printf("Printing message because this is in do-while statement\n");
+    /* Infinity loop */
   } while(i=1);
 }
